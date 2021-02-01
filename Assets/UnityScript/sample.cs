ddbcf0b8a86ecd2feee52ac18a0c60025db04b24
chonommmm <kdg.chono.mikiya@gmail.com>
2021-02-01 11:27:11 +0900

    add texture

--- Assets/UnityScript/sample.cs
+++ Assets/UnityScript/sample.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class sample : MonoBehaviour
 {
+    [SerializeField] GameObject roomPrefab=null;
+    const int LEFT=1,DOWN=2,RIGHT=4,UP=8;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,39 @@ public class sample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            VectorControl(1);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            VectorControl(2);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)){
+            VectorControl(4);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha8)){
+            VectorControl(8);
+        }
         
+    }
+    void VectorControl(int value){
+        GameObject room;
+        if(value==DOWN||value==UP||value==RIGHT||value==LEFT){
+            switch(value){
+                case (DOWN):
+                    room = Instantiate(roomPrefab,new Vector3(10,0,10),Quaternion.Euler(0,180,0)) as GameObject;
+                    break;
+                case (UP):
+                    room = Instantiate(roomPrefab,new Vector3(10,0,10),Quaternion.Euler(0,0,0)) as GameObject;
+                    break;
+                case (RIGHT):
+                    room = Instantiate(roomPrefab,new Vector3(10,0,10),Quaternion.Euler(0,90,0)) as GameObject;
+                    break;
+                case (LEFT):
+                    room = Instantiate(roomPrefab,new Vector3(10,0,10),Quaternion.Euler(0,270,0)) as GameObject;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
