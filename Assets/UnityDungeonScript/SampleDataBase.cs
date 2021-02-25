aeec59f6bbb14d4b54a23fb727e32f1e0ba17ad2
chonomikiya1 <kdg.chono.mikiya@gmail.com>
2021-02-25 16:20:11 +0900

    edit sqlite table

--- Assets/UnityDungeonScript/SampleDataBase.cs
+++ Assets/UnityDungeonScript/SampleDataBase.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using System.Collections.Generic;
 /*
+SCHEMA
+
 CREATE TABLE itemtable(id INTEGER primary key,item TEXT,sell INTEGER);
 CREATE TABLE money(total INTEGER);
 CREATE TABLE modifiertable(id INTEGER primary key,modifier TEXT,magnification REAL);
-Englishnoobな自分へ                               ,装飾子        ,倍率
+English noobな自分へ                               ,装飾子        ,倍率
+CREATE TABLE mazetable (id INTEGER primary key,name TEXT,seed INTEGER);
 */
 
 public class SampleDataBase : MonoBehaviour {
     public SqliteDatabase SqliteDatabase;
     private const string itemtable = "itemtable";
     private void Start() {
+        SelectFromExampleTable();
+        SelectFromItemTable();
+        }
+
+    void SelectFromExampleTable(){
         SqliteDatabase = new SqliteDatabase("default.db");
         string query = "SELECT name,dummy FROM example";
         // string query1 = "insert into example values(\"script\",3)";
@@ -19,15 +27,14 @@ public class SampleDataBase : MonoBehaviour {
         DataTable dt = SqliteDatabase.ExecuteQuery(query);
         string name;
         int dummy;
+        Debug.Log("example = "+dt.Rows.Count);
         foreach(DataRow dr in dt.Rows){
             name = (string)dr["name"];
             dummy = (int)dr["dummy"];
             Debug.Log("name: "+name.ToString());
             Debug.Log("dummy: "+dummy);
-            }
-        SelectFromItemTable();
         }
-
+    }
     
     void SelectFromItemTable(){
         SqliteDatabase = new SqliteDatabase("default.db");
@@ -37,6 +44,7 @@ public class SampleDataBase : MonoBehaviour {
         int _id;
         string _item;
         int _sell;
+        Debug.Log("itemtable = "+dt.Rows.Count);
         foreach(DataRow dr in dt.Rows){
             _id = (int)dr["id"];
             _item = (string)dr["item"];
