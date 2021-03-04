45f14dfd40bf991b79998dbc55de59848194fb68
chonomikiya1 <kdg.chono.mikiya@gmail.com>
2021-03-04 16:36:56 +0900

    work on base script

---
+++ Assets/UnityDungeonScript/UI/BaseUiCtl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUiCtl : MonoBehaviour
+{
+    [SerializeField] GameObject input_ui_prefab = null;
+    [SerializeField] Transform SeedInputButton = null;
+    GameObject seed_input_ui = null;    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCreateSeedInputUi(){
+        seed_input_ui = Instantiate(input_ui_prefab ) as GameObject;
+    }
+    public void OnExitButtonDestory(){
+        Destroy(this.seed_input_ui.gameObject);
+    }
+}
---
+++ Assets/UnityDungeonScript/UI/InputCanvasCtl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputCanvasCtl : MonoBehaviour
+{
+    [SerializeField] InputField inputField = null;   
+    [SerializeField] string inputtext; 
+    const int TEXT_MAX_LENGTH = 6;
+    bool isparse  = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //InputFieldの入力をstringへ格納
+    public void OnInputSeedValue(){
+        inputtext = inputField.text;
+        Debug.Log(CheckSeedValueBeUse(inputtext));
+        ParseStringToInt(inputtext);
+    }
+    //入力をSeedへ格納できるかcheck
+    public bool CheckSeedValueBeUse(string _text){
+        bool _isparse = false;
+        if(_text.Length < TEXT_MAX_LENGTH){
+            _isparse = true;
+        }
+
+        return _isparse;
+    }
+    public int ParseStringToInt(string _text){
+        int _seed = 0;
+        string islink = "";
+        char [] char_parse = _text.ToCharArray();
+        foreach (char val in char_parse){
+            Debug.Log(val);
+            islink = islink + val;
+        }
+        Debug.Log(islink);
+        return _seed;
+    }
+    public void OnExitSeedInputUi(){
+        Destroy(this.transform.root.gameObject);
+    }
+}
