a78c18a1d9711821454263c3be157c1c9b639b1f
chonommmm <kdg.chono.mikiya@gmail.com>
2021-03-17 04:12:45 +0900

    add result ui read database

---
+++ Assets/UnityDungeonScript/result/ItemRow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemRow : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI itemTMPro = null;
+    [SerializeField] TextMeshProUGUI priceTMPro = null;
+    public void SetItemText(string _text){
+        itemTMPro.text = _text;
+    }
+    public void SetPriceText(string _text){
+        priceTMPro.text = _text;
+    }
+    public void ChangePosition(Vector3 _vector3){
+        // Vector3 tmp = this.transform.position;
+        this.transform.position += _vector3;
+    }
+
+}
--- Assets/UnityDungeonScript/result/ResultUi.cs
+++ Assets/UnityDungeonScript/result/ResultUi.cs
@@ -1,19 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ResultUi : MonoBehaviour
 {
+    //Rowが多い場合はContentのHeightを加える
     [SerializeField] GameObject Content = null;
+    [SerializeField] GameObject RowPrefab = null;
+    SqliteDatabase m_sqlitedatabase;
+    const int UI_OFFSET = -25;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_sqlitedatabase = new SqliteDatabase("default.db");
+        GetUserPossession();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)){
+            // GameObject AddRow = Instantiate(RowPrefab,Content.transform.position,Quaternion.identity) as GameObject;
+            GameObject AddRow = Instantiate(RowPrefab) as GameObject;
+            AddRow.transform.SetParent(Content.transform,false);
+        }
+    }
+    public void GetUserPossession(){
+        DataTable dt = m_sqlitedatabase.ExecuteQuery("SELECT id,item,sell FROM user_possession");
+        int count = 1;
+        foreach(DataRow dr in dt.Rows){
+            GameObject AddRow = Instantiate(RowPrefab) as GameObject;
+            AddRow.name = "Row_"+count;
+            AddRow.transform.SetParent(Content.transform,false);
+            AddRow.GetComponent<ItemRow>().ChangePosition(new Vector3(0,UI_OFFSET*count,0));
+            AddRow.GetComponent<ItemRow>().SetItemText((string)dr["item"]);
+            AddRow.GetComponent<ItemRow>().SetPriceText(dr["sell"].ToString());
+            Debug.Log(dr["id"]);
+            Debug.Log(dr["item"]);
+            Debug.Log(dr["sell"]);
+            count++;
+        }
+    }
+    public void ChangeSceneBase(){
+        SceneManager.LoadScene("base");
     }
 }
